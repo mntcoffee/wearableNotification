7567eda7292fe0498889fd0010c12065a7901126
stakasaki <takasaki.shogo707@mail.kyutech.jp>
2021-04-26 13:06:40 +0900

    created necessary files(blank)

---
+++ app/src/main/java/com/example/wearablenotification/setup/SetupActivity.kt
@@ -0,0 +1,12 @@
+package com.example.wearablenotification.setup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.wearablenotification.R
+
+class SetupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setup)
+    }
+}
