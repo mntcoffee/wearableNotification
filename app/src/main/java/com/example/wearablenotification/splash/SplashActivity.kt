0d7e466d92a41d10259a19da156bad676769c9ee
stakasaki <takasaki.shogo707@mail.kyutech.jp>
2021-04-27 16:04:01 +0900

    created splash activity

---
+++ app/src/main/java/com/example/wearablenotification/splash/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.example.wearablenotification.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.wearablenotification.R
+import com.example.wearablenotification.setup.SetupActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(3000)
+            val intent = Intent(this@SplashActivity, SetupActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
